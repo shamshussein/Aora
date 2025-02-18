9fa50ae04acc6c95c1f7b575dfc38b6a2b918559
shamshussein <hussein_shams@outlook.com>
2025-02-18 02:21:37 +0200

    sign in setup and components

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.logo}
+          resizeMode='contain'
+          className="w-[115px] h-[35px]"/>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Login to Aora
+          </Text>
+          
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default SignIn
-
-const styles = StyleSheet.create({})
